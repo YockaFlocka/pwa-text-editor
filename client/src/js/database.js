34a9aabe12034435798ef6fe52d7caaba2f70b4c
Peter Yockey <peterjyockey@gmail.com>
2023-05-29 18:37:31 -0500

    small changes

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,21 +17,20 @@ export const putDb = async (content) => {
   console.error('putDb not implemented');
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('contact', 1);
+  const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readwrite');
+  const tx = jateDb.transaction('jate', 'readwrite');
 
   // Open up the desired object store.
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore('jate');
 
-  // use the .update() method to update db
-  const request = store.update();
+  // use the .put() method to update db
+  const request = store.put({ id: 1, value: content });
 
   // Get confirmation of the request.
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  console.log('Data saved to the database', result.value);
 };
 
 
@@ -39,21 +38,23 @@ export const getDb = async () => {
   console.error('getDb not implemented');
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('contact', 1);
+  const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readonly');
+  const tx = jateDb.transaction('jate', 'readonly');
 
   // Open up the desired object store.
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
   const request = store.getAll();
 
   // Get confirmation of the request.
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  result
+    ? console.log('data retrieved from the database', result.value)
+    : console.log("data not found in the database")
+  return result?.value;
 };  
 
 initdb();
